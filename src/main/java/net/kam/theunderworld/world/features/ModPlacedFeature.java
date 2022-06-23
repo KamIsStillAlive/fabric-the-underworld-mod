aa5e0ede2531ae2d0212764d9def633b49ae3c65
JaimeG <jaimeeduardo2009@gmail.com>
2022-06-23 09:57:01 -0500

    World gen

--- src/main/java/net/kam/theunderworld/world/features/ModPlacedFeature.java
+++ src/main/java/net/kam/theunderworld/world/features/ModPlacedFeature.java
@@ -4,12 +4,17 @@ import net.minecraft.util.registry.RegistryEntry;
 import net.minecraft.world.gen.YOffset;
 import net.minecraft.world.gen.feature.PlacedFeature;
 import net.minecraft.world.gen.feature.PlacedFeatures;
+import net.minecraft.world.gen.placementmodifier.CountPlacementModifier;
 import net.minecraft.world.gen.placementmodifier.HeightRangePlacementModifier;
+import net.minecraft.world.gen.placementmodifier.SquarePlacementModifier;
+
+import java.util.Arrays;
 
 public class ModPlacedFeature {
 
     public static final RegistryEntry<PlacedFeature> PLATINUM_ORE_PLACED = PlacedFeatures.register("platinum_ore_placed",
-            ModConfiguredFeatures.PLATINUM_ORE, ModOreFeatures.modifiersWithCount(7,
-                    HeightRangePlacementModifier.trapezoid(YOffset.aboveBottom(-80), YOffset.aboveBottom(80))));
-
+            ModConfiguredFeatures.PLATINUM_ORE, Arrays.asList(
+                    CountPlacementModifier.of(15), SquarePlacementModifier.of(),
+                    HeightRangePlacementModifier.uniform(YOffset.getBottom(), YOffset.fixed(64))
+            ));
 }

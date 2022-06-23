8894f1c962d5d7b4b37f02abeb4f5045d1e52afc
JaimeG <jaimeeduardo2009@gmail.com>
2022-06-23 10:50:53 -0500

    Virulent Sapling Render

--- src/main/java/net/kam/theunderworld/block/ModBlocks.java
+++ src/main/java/net/kam/theunderworld/block/ModBlocks.java
@@ -3,11 +3,15 @@ package net.kam.theunderworld.block;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.kam.theunderworld.TheUnderworld;
+import net.kam.theunderworld.block.custom.ModSaplingBlock;
 import net.kam.theunderworld.item.ModItemGroup;
 import net.minecraft.block.*;
+import net.minecraft.block.sapling.AcaciaSaplingGenerator;
+import net.minecraft.block.sapling.OakSaplingGenerator;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -37,16 +41,17 @@ public class ModBlocks {
     public static final Block VIRULENT_LEAVES = registerBlock("virulent_leaves",
             new LeavesBlock(FabricBlockSettings.copy(Blocks.OAK_LEAVES).nonOpaque()), ModItemGroup.THEUNDERWORLD);
     public static final Block VIRULENT_SAPLING = registerBlock("virulent_sapling",
-            new Block(FabricBlockSettings.copy(Blocks.OAK_LEAVES).nonOpaque()), ModItemGroup.THEUNDERWORLD);
+            new ModSaplingBlock(new AcaciaSaplingGenerator(), //Change AcaciaSaplingGenerator with the actual tree sapling generator
+                    FabricBlockSettings.copy(Blocks.OAK_SAPLING)), ModItemGroup.THEUNDERWORLD);
 
 
     private static Block registerBlock(String name, Block block, ItemGroup group) {
         registerBlockItem(name, block, group);
-        return net.minecraft.util.registry.Registry.register(Registry.BLOCK, new Identifier(TheUnderworld.MOD_ID, name), block);
+        return Registry.register(Registry.BLOCK, new Identifier(TheUnderworld.MOD_ID, name), block);
     }
 
     private static Item registerBlockItem(String name, Block block, ItemGroup group) {
-        return net.minecraft.util.registry.Registry.register(Registry.ITEM, new Identifier(TheUnderworld.MOD_ID, name),
+        return Registry.register(Registry.ITEM, new Identifier(TheUnderworld.MOD_ID, name),
                 new BlockItem(block, new FabricItemSettings().group(group)));
     }
 

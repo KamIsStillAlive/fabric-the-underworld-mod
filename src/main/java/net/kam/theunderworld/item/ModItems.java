64733cb5341f462b8c61e28e1c807ca6c2f4658b
JaimeG <jaimeeduardo2009@gmail.com>
2022-06-23 11:15:08 -0500

    Platinum Armor

--- src/main/java/net/kam/theunderworld/item/ModItems.java
+++ src/main/java/net/kam/theunderworld/item/ModItems.java
@@ -2,6 +2,10 @@ package net.kam.theunderworld.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.kam.theunderworld.TheUnderworld;
+import net.kam.theunderworld.item.custom.ModArmorMaterial;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -15,6 +19,23 @@ public class ModItems {
 
     public static final Item FORBIDDEN_FOSSIL = registerItem("forbidden_fossil",
             new Item(new FabricItemSettings().group(ModItemGroup.THEUNDERWORLD)));
+
+    //Armor
+    public static final Item PLATINUM_HELMET = registerItem("platinum_helmet",
+            new ArmorItem(ModArmorMaterial.PLATINUM, EquipmentSlot.HEAD,
+                    new Item.Settings().group(ModItemGroup.THEUNDERWORLD)));
+
+    public static final Item PLATINUM_CHESTPLATE = registerItem("platinum_chestplate",
+            new ArmorItem(ModArmorMaterial.PLATINUM, EquipmentSlot.CHEST,
+                    new Item.Settings().group(ModItemGroup.THEUNDERWORLD)));
+
+    public static final Item PLATINUM_LEGGINGS = registerItem("platinum_leggings",
+            new ArmorItem(ModArmorMaterial.PLATINUM, EquipmentSlot.LEGS,
+                    new Item.Settings().group(ModItemGroup.THEUNDERWORLD)));
+
+    public static final Item PLATINUM_BOOTS = registerItem("platinum_boots",
+            new ArmorItem(ModArmorMaterial.PLATINUM, EquipmentSlot.FEET,
+                    new Item.Settings().group(ModItemGroup.THEUNDERWORLD)));
 
     private static Item registerItem(String name, Item item) {
         return net.minecraft.util.registry.Registry.register(Registry.ITEM, new Identifier(TheUnderworld.MOD_ID, name), item);

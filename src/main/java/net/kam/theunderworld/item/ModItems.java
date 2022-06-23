3661f487db28fe1889b0728d4844b9b9fd1530e4
JaimeG <jaimeeduardo2009@gmail.com>
2022-06-23 16:44:25 -0500

    Platinum tools :D

--- src/main/java/net/kam/theunderworld/item/ModItems.java
+++ src/main/java/net/kam/theunderworld/item/ModItems.java
@@ -2,11 +2,9 @@ package net.kam.theunderworld.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.kam.theunderworld.TheUnderworld;
-import net.kam.theunderworld.item.custom.ModArmorMaterial;
+import net.kam.theunderworld.item.custom.*;
 import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.item.ArmorItem;
-import net.minecraft.item.ArmorMaterial;
-import net.minecraft.item.Item;
+import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -36,6 +34,28 @@ public class ModItems {
     public static final Item PLATINUM_BOOTS = registerItem("platinum_boots",
             new ArmorItem(ModArmorMaterial.PLATINUM, EquipmentSlot.FEET,
                     new Item.Settings().group(ModItemGroup.THEUNDERWORLD)));
+
+    // Tools
+
+    public static final Item PLATINUM_SWORD = registerItem("platinum_sword",
+            new SwordItem(ModToolMaterial.PLATINUM, 4, 3f,
+                    new Item.Settings().group(ModItemGroup.THEUNDERWORLD)));
+
+    public static final Item PLATINUM_PICKAXE = registerItem("platinum_pickaxe",
+            new ModPickaxeItem(ModToolMaterial.PLATINUM, 1, 2f,
+                    new FabricItemSettings().group(ModItemGroup.THEUNDERWORLD)));
+
+    public static final Item PLATINUM_AXE = registerItem("platinum_axe",
+            new ModAxeItem(ModToolMaterial.PLATINUM, 6, 1f,
+                    new FabricItemSettings().group(ModItemGroup.THEUNDERWORLD)));
+
+    public static final Item PLATINUM_HOE = registerItem("platinum_hoe",
+            new ModHoeItem(ModToolMaterial.PLATINUM, 0, 0f,
+                    new FabricItemSettings().group(ModItemGroup.THEUNDERWORLD)));
+
+    public static final Item PLATINUM_SHOVEL = registerItem("platinum_shovel",
+            new ShovelItem(ModToolMaterial.PLATINUM, 1, 2f,
+                    new FabricItemSettings().group(ModItemGroup.THEUNDERWORLD)));
 
     private static Item registerItem(String name, Item item) {
         return net.minecraft.util.registry.Registry.register(Registry.ITEM, new Identifier(TheUnderworld.MOD_ID, name), item);

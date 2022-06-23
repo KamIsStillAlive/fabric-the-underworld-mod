94d5b18787c762dff3a5091b28b49d46fec9879e
JaimeG <jaimeeduardo2009@gmail.com>
2022-06-23 10:26:05 -0500

    Virulent Sapling Render

--- src/main/java/net/kam/theunderworld/TheUnderworld.java
+++ src/main/java/net/kam/theunderworld/TheUnderworld.java
@@ -1,14 +1,17 @@
 package net.kam.theunderworld;
 
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.kam.theunderworld.block.ModBlocks;
 import net.kam.theunderworld.item.ModItems;
 import net.kam.theunderworld.util.ModRegistries;
 import net.kam.theunderworld.world.gen.ModWorldGen;
+import net.minecraft.client.render.RenderLayer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class TheUnderworld implements ModInitializer {
+public class TheUnderworld implements ModInitializer, ClientModInitializer {
 	public static final String MOD_ID = "theunderworld";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
@@ -25,4 +28,8 @@ public class TheUnderworld implements ModInitializer {
 		LOGGER.info("Hello Fabric world!");
 	}
 
+	@Override
+	public void onInitializeClient() {
+		BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.VIRULENT_SAPLING, RenderLayer.getCutout());
+	}
 }
